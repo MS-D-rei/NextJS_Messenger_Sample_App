7f01f7233e0277c79a4f8e87d295252db6c736b8
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-05-18 02:51:43 +0900

    add justify-center to app/(site)/page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,15 +1,16 @@
-import Image from "next/image";
+import Image from 'next/image';
+import messengerLogo from '@/public/images/Messenger-Logo-PNG.png';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-full py-12 sm:px-6 lg:px-8 bg-gray-100">
+    <div className="flex flex-col justify-center min-h-full py-12 sm:px-6 lg:px-8 bg-gray-100">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
           alt="logo"
-          height={48}
-          width={48}
+          height={100}
+          width={100}
           className="mx-auto w-auto"
-          src="/images/Messenger-Logo-PNG.png"
+          src={messengerLogo}
         ></Image>
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
           Sign in your account
