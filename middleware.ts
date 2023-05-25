cdd3337942927d9e6b60d66412ccb765beb9634a
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-05-25 22:24:46 +0900

    create middleware to protect route

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { withAuth } from 'next-auth/middleware';
+
+export default withAuth({
+  pages: {
+    signIn: '/',
+  },
+})
+
+export const config = {
+  // protect route which is matched with the matcher below
+  matcher: [
+    "/users/:path*",
+  ],
+}
