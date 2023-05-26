1a605a330fdf7ccad41b10d6b3aaa47cbe32600a
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-05-26 20:35:32 +0900

    create DesktopSidebarItem component

---
+++ app/components/sidebar/DesktopSidebarItem.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import clsx from 'clsx';
+import { group } from 'console';
+import Link from 'next/link';
+import { IconType } from 'react-icons';
+
+interface DesktopSidebarItemProps {
+  label: string;
+  href: string;
+  icon: IconType;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+const DesktopSidebarItem: React.FC<DesktopSidebarItemProps> = ({
+  label,
+  href,
+  icon: Icon,
+  active,
+  onClick,
+}) => {
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+  return (
+    <li key={label} onClick={handleClick}>
+      <Link
+        href={href}
+        className={clsx(
+          `
+          flex
+          group
+          rounded-md
+          gap-x-3
+          text-sm
+          leading-6
+          font-semibold
+          text-gray-500
+          p-3
+          hover:text-black
+          hover:bg-gray-100
+          `,
+          active && `bg-gray-100 text-black`
+        )}
+      >
+        <Icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+        <span className="sr-only">{label}</span>
+      </Link>
+    </li>
+  );
+};
+
+export default DesktopSidebarItem;
