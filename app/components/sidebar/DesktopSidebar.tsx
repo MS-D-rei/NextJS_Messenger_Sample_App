2c80a4937e90abfa00eebf90bceb4c32a8c823ff
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-05-26 20:35:58 +0900

    apply DesktopSidebarItem to DesktopSidebar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,7 +1,56 @@
+'use client';
+
+import { useState } from "react";
+import { useRoutes } from "@/app/hooks/useRoutes";
+import DesktopSidebarItem from "@/app/components/sidebar/DesktopSidebarItem";
+
 function DesktopSidebar() {
+  const routes = useRoutes();
+  // const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-      DesktopSidebar
+    <div className="
+      hidden
+      lg:flex
+      lg:flex-col
+      justyfy-between
+      lg:fixed
+      lg:inset-y-0
+      lg:left-0
+      lg:z-40
+      lg:w-20
+      lg:overflow-auto
+      lg:bg-white
+      lg:border-r-[1px]
+      lg:pb-4
+      xl:px-6
+      ">
+      <nav className="
+        flex
+        flex-col
+        justify-between
+        mt-4
+        ">
+        <ul
+          role='list'
+          className="
+          flex
+          flex-col
+          items-center
+          space-y-4
+          "
+        >
+          {routes.map((route) => (
+            <DesktopSidebarItem
+              key={route.label}
+              href={route.href}
+              label={route.label}
+              icon ={route.icon}
+              active={route.active}
+            />
+          ))}
+        </ul>
+      </nav>
     </div>
   )
 }
