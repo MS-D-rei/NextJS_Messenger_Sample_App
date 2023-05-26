852b58117923c95c06b9cf310af1c47afc98fe71
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-05-27 00:52:54 +0900

    create getCurrentUser to current user with session

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,26 @@
+import prisma from '@/app/libs/prismadb';
+import { getSession } from 'next-auth/react';
+
+export const getCUrrentUser = async () => {
+  try {
+    const session = await getSession();
+    if (!session?.user?.email) {
+      return null;
+    }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email,
+      },
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return currentUser;
+  } catch (err: any) {
+    console.log(err);
+    return null;
+  }
+};
