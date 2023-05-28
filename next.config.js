3fd06f638503f153f0e9a5baee01cfc2be5736e4
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-05-28 17:41:38 +0900

    add image setting to allow google and github image in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      // allow google and github images
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**/*',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**/*',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
