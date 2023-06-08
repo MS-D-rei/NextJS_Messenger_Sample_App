6f7b91161964569504a0ec4023a26d703756859b
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-06-08 22:01:47 +0900

    create Modal component as a base one

---
+++ app/components/Modal.tsx
@@ -0,0 +1,65 @@
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment } from 'react';
+import { IoClose } from 'react-icons/io5';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  return (
+    <Transition.Root show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-50" onClose={onClose}>
+        {/* gray schreen */}
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        {/* modal */}
+        <div className='fixed inset-0 z-10 overflow-y-auto'>
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel
+                className="relative w-full transform overflow-hidden rounded-lg bg-white px-4 pb-4 text-left transition-all
+                sm:my-8 sm:max-w-lg sm:p-6"
+              >
+                <div className="absolute hidden top-0 right-0 pt-4 pr-4 z-10 sm:block">
+                  <button
+                    type="button"
+                    onClick={onClose}
+                    className="rounded-md bg-white text-gray-400 hover:text-gray-500
+                    focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+                  >
+                    <span className="sr-only">Close</span>
+                    <IoClose className="h-6 w-6" />
+                  </button>
+                </div>
+                {children}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
+
+export default Modal;
