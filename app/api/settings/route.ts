973203edc09aacc17c97b10a453e0b86a9003871
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-06-10 21:06:52 +0900

    add logic to update user info in settings/route.ts

--- app/api/settings/route.ts
+++ app/api/settings/route.ts
@@ -1,17 +1,32 @@
-import { getCurrentUser } from "@/app/actions/getCurrentUser";
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
+import prisma from '@/app/libs/prismadb';
+import { getCurrentUser } from '@/app/actions/getCurrentUser';
 
 export async function POST(request: Request) {
   try {
     const currentUser = await getCurrentUser();
-    const isUnAuthorized = !currentUser?.id || !currentUser.email
+    const isUnAuthorized = !currentUser?.id || !currentUser.email;
 
     if (isUnAuthorized) {
-      return new NextResponse('Unauthorized', { status: 400 })
+      return new NextResponse('Unauthorized', { status: 400 });
     }
 
+    const body = await request.json();
+    const { name, image } = body;
+
+    const updateUser = await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        name,
+        image,
+      },
+    });
+
+    return NextResponse.json(updateUser);
   } catch (err) {
     console.log(err);
-    return new NextResponse('Internal server error', { status: 500 });
+    return new NextResponse('Setting route error', { status: 500 });
   }
 }
