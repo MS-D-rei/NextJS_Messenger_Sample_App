39caa797fb8e76ee763818db1d4ec3ee4167ad3c
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-06-13 16:23:49 +0900

    add LoadingModal to UserListItem

--- app/users/components/UserListItem.tsx
+++ app/users/components/UserListItem.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation';
 import { useCallback, useState } from 'react';
 import { User } from '@prisma/client';
 import Avatar from '@/app/components/Avatar';
+import LoadingModal from '@/app/components/LoadingModal';
 
 interface UserListItemProps {
   data: User;
@@ -27,33 +28,36 @@ const UserListItem: React.FC<UserListItemProps> = ({ data }) => {
   }, [data.id, router]);
 
   return (
-    <div
-      onClick={handleClick}
-      className="
-      relative
-      w-full
-      flex
-      items-center
-      space-x-3
-      bg-white
-      p-3
-      hover:bg-neutral-100
-      rounded-lg
-      transition
-      cursor-pointer
-      "
-    >
-      <Avatar user={data} />
+    <>
+      {isLoading && <LoadingModal />}
+      <div
+        onClick={handleClick}
+        className="
+        relative
+        w-full
+        flex
+        items-center
+        space-x-3
+        bg-white
+        p-3
+        hover:bg-neutral-100
+        rounded-lg
+        transition
+        cursor-pointer
+        "
+      >
+        <Avatar user={data} />
 
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-none">
-          <span className="absolute inset-0" aria-hidden="true" />
-          <div className="flex items-center justify-between mb-1">
-            <p className="text-sm font-medium text-gray-900">{data.name}</p>
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-none">
+            <span className="absolute inset-0" aria-hidden="true" />
+            <div className="flex items-center justify-between mb-1">
+              <p className="text-sm font-medium text-gray-900">{data.name}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
